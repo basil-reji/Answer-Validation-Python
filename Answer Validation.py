2b6eb26ee35d98dafd1074114b5e6bacd9f19c30
Basil Reji <basil-reji@users.noreply.github.com>
2021-05-28 11:12:39 +0530

    Modified the code

--- Answer Validation.py
+++ Answer Validation.py
@@ -1,18 +1,28 @@
+from os import path
 import pandas as pd
 
-df =pd.read_excel('Answers.xlsx',engine='openpyxl')
+path_xslx=input("Enter the path of the Answers excel file: ")
 
-answers = ['free()','#include','* / % + - =','ZeroHello World','Error: expression syntax','Program prints hii 1 time.','float','Compile error','It is a type name defined in stdio.h','2','6','Opens text file in both reading and appending mode','Runs infinitely without printing anything','return','#define CUBE(X) (X*X*X)','In first statement, 4 specifies an array size, whereas in second statement it specifies a particular element of array','5','Runtime error','1,2,3,4','Infinite loop']
+df =pd.read_excel(path_xslx ,engine='openpyxl')
+
+n=int(input("Enter the count of questions: "))
+loc=input("Enter the Path of answer's: ")
+
+file=open(loc,'r')
+answers=[]
+
+for line in file:
+    answers.append(line)
 
 q=[]
-for i in range(20):
+for i in range(n):
     q.append('Q'+str(i+1))
 
 m_c=[]
-for i in range(20):
+for i in range(n):
     m_c.append('Mark'+str(i+1))
 
-for j in range(20):
+for j in range(n):
     for i, row in df.iterrows():
         ans = answers[j]
         predicted1 = df.loc[i,q[j]]
@@ -20,6 +30,6 @@ for j in range(20):
             s=m_c[j]
             df.loc[i,s] = 1
 
-writer = pd.ExcelWriter('new_book.xlsx')
-df.to_excel(writer,'new_sheet')
+writer = pd.ExcelWriter('Validated Book.xlsx')
+df.to_excel(writer,'Validated_sheet')
 writer.save()
